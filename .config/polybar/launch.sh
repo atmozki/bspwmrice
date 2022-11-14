95b13fec8aa94d824e1c3df15c8c709290dff8f0
Dennis Jojo Kuriakose <62955521+atmozki@users.noreply.github.com>
2022-11-14 23:23:14 +0530

    comment edited

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,7 +6,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
-# Start mainbar
+# Start Mainbar
 polybar mainbar &
 
 # Start external display mainbar
